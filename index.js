8b552ef6404fe589be1589cfa05a6f30c83e5314
Christian Treppo <christian@treppo.org>
2015-05-25 18:20:12 +0200

    Link rating to imdb page

--- index.js
+++ index.js
@@ -1,63 +1,57 @@
 (global => {
+  let unbox = f => p => p.then(f);
+  let proxify = url => 'http://crossorigin.me/' + url;
+  let unproxify = url => url.replace(/http:\/\/crossorigin.me\//, '');
+
   let fetch = (url) => {
+    if (!url) { return Promise.resolve() };
+
     return new Promise((resolve, reject) => {
       let xhr = new XMLHttpRequest();
       xhr.onload = () => {
         if (xhr.status == 200) { resolve(xhr.responseXML) }
         else { reject(Error(xhr.statusText)) } };
-      xhr.onError = () => reject("XHR Error");
-      xhr.open("GET", url);
+      xhr.onError = reject;
+      xhr.open("GET", proxify(url));
       xhr.responseType = "document";
       xhr.overrideMimeType("text/html");
       xhr.send() }) };
 
   let getYorckMovies = () => {
-    const url = "http://crossorigin.me/http://www.yorck.de/mobile/filme";
+    const url = "http://www.yorck.de/mobile/filme";
     let getMovieList = page => Array.prototype.slice.call(page.querySelectorAll('.films a')).map(_ => _.textContent)
     let fetchListOfMovies = () => fetch(url).then(getMovieList);
 
     return fetchListOfMovies() };
 
-  // let zip = () => {
-  //   let args = Array.from(arguments);
-  //
-  //   return args[0].map((_, i) => args.map(array => array[i]));
-  // };
-
   let getImdbPages = movies => {
-    const url = "http://crossorigin.me/http://www.imdb.com";
+    const url = "http://www.imdb.com";
     const urls = movies.map(m => url + "/find?s=tt&q="+ encodeURIComponent(m));
 
     let getMovieUrl = page => {
       let a = page.querySelector('.findList .result_text a');
-      if (!a) { return null };
+      if (!a) { return };
       return url + a.pathname };
 
-    let fetchUrlsOfMovies = urls.map(url => fetch(url).then(getMovieUrl, console.error));
+    let fetchUrlsOfMovies = urls.map(url => fetch(url)).map(unbox(getMovieUrl));
 
     return fetchUrlsOfMovies };
 
-  let getMovieWithRating = pageUrls => {
-    let fetchPage = urlPromise => {
-      return urlPromise.then(url => { if (url) { return fetch(url) }})
-    };
+  let getMovieWithRating = (moviesWithUrls) => {
     let extractInfos = page => {
-      if (!page) { return null };
+      if (!page) { return };
       let title = page.querySelector('#overview-top .header').textContent;
       let rating = page.querySelector('#overview-top .star-box div').textContent;
-      return title + ' ' + rating };
-    return Promise.all(pageUrls.map(fetchPage).map(pagePromise => pagePromise.then(extractInfos)));
-  }
+      return [title, rating, unproxify(page.URL)] };
+    return moviesWithUrls.map(unbox((url) => fetch(url))).map(unbox((page) => extractInfos(page)));
+  };
 
-  let showOnPage = () => {
-    let moviesEl = document.getElementById("movies");
-    let args = Array.from(arguments);
-
-    moviesEl.textContent = args; };
+  let moviesEl = document.getElementById("movies");
+  let showOnPage = movie => moviesEl.innerHTML += `${movie[0]} <a href='${movie[2]}'>${movie[1]}</a><br>`;
 
   let movies = getYorckMovies();
   let pages = movies.then(getImdbPages);
   let ratings = pages.then(getMovieWithRating);
 
-  ratings.then(showOnPage);
+  ratings.then(v => v.map(unbox(showOnPage)));
 })(this);
